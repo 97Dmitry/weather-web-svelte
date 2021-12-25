38f3d1142beaf0a7307da3c699d49c95c7756091
97Dmitry <dart_veyder_1997@mail.ru>
2021-12-25 15:13:49 +0600

    feat: api has been changed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
-  import ApiService, { Lang } from "../services/ApiService";
-  import degreesConversion from "../utils/degreesConversion";
-  import { WeatherResponseData } from "../types/weatherResponseData";
+  import ApiService, { LANG, CITIES } from "../services/ApiService";
 
-  const weather = new ApiService("omsk", Lang.en);
+  const weather = new ApiService(CITIES.OMSK, LANG.RU);
 
-  let currentWeather: string;
+  let currentWeather: number;
+  let city: string;
+  let weatherAbout: string;
 
   weather
     .getCurrentWeather()
-    .then((data: WeatherResponseData) => {
-      currentWeather = degreesConversion(data.data.main.temp);
+    .then(({ data: { current, location } }) => {
+      currentWeather = current.temp_c;
+      city = location.name;
+      weatherAbout = current.condition.text;
     })
     .catch((error) => console.log(error));
 </script>
 
 <main>
-  <p class="currentDegrees">Текущая погода: {currentWeather}</p>
+  <p class="currentDegrees">
+    Текущая погода в городе {city}: {currentWeather}, {weatherAbout}
+  </p>
 </main>
 
 <style>
--- src/utils/degreesConversion.ts
+++
@@ -1,3 +0,0 @@
-export default function (kelvin: number) {
-  return (Number(kelvin) - 273.15).toFixed(2);
-}
